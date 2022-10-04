bb8c741ae4542f01a2c406ae18ef6bea3947e5d4
EC2 Default User <ec2-user@ip-172-31-47-44.ap-northeast-1.compute.internal>
2022-10-04 13:19:38 +0000

    [Add] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,15 +10,19 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@post_book.id)
     else
-      redirect_to books_path
+      @books = Book.all
+      render :index
     end
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book_update = Book.find(params[:id])
+    if @book_update.update(book_params)
       flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book_update.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
     end
   end
 
@@ -30,11 +34,13 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @post_book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_n = Book.new
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = Book.where(user_id: params[:id])
+    @book = Book.new
   end
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def edit
